e51512f00860b10ae37d4c7ffa03de7288e9f171
Michael Peralta <peralta.michael27@gmail.com>
2024-01-06 19:32:26 -0500

    Detect when R and L paddle misses the ball

--- ball.py
+++ ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.x_move = 10
         self.y_move = 10
 
+
     def move(self) -> None:
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
@@ -22,4 +23,6 @@ class Ball(Turtle):
     def bounce_x(self) -> None:
         self.x_move *= -1
 
-
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-import  time
+import time
 
 
-r_paddle = Paddle((370, 0))
-l_paddle = Paddle((-370, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 ball = Ball()
 
 
@@ -29,13 +29,21 @@ while game_is_on:
     ball.move()
 
     # Detecting Collision
-    if ball.ycor() > 280 or ball.ycor() < -280 :
+    if ball.ycor() > 280 or ball.ycor() < -280:
         # needs to bounce
         ball.bounce_y()
 
     # Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    # Detect when R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
